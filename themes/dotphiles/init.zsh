caa6bc225e5318a430ca7b2ed637a020a4128884
Ben O'Hara <bohara@gmail.com>
2012-07-22 16:31:59 +1000

    Can use zenburn colors with the dotphiles theme

--- themes/dotphiles/init.zsh
+++ themes/dotphiles/init.zsh
@@ -7,8 +7,17 @@
 #   Ben O'Hara <bohara@gmail.com>
 #
 
-zstyle ':dotzsh:module:theme:dircolors:dotphiles' theme 'dircolors/dircolors.256dark'
-zstyle ':dotzsh:module:theme:lscolors:dotphiles' theme 'gxfxbEaEBxxEhEhBaDaCaD'
+if [[ "$_theme[2]" = "zenburn" ]]; then
+  zstyle ':dotzsh:module:theme:dircolors:dotphiles' theme 'dircolors/dircolors.zenburn'
+  zstyle ':dotzsh:module:theme:lscolors:dotphiles' theme 'gxfxbEaEBxxEhEhBaDaCaD'
+elif [[ "$_theme[2]" = "sollight" ]]; then
+  zstyle ':dotzsh:module:theme:dircolors:dotphiles' theme 'dircolors/dircolors.ansi-light'
+  zstyle ':dotzsh:module:theme:lscolors:dotphiles' theme 'gxfxbEaEBxxEhEhBaDaCaD'
+else
+  zstyle ':dotzsh:module:theme:dircolors:dotphiles' theme 'dircolors/dircolors.256dark'
+  zstyle ':dotzsh:module:theme:lscolors:dotphiles' theme 'gxfxbEaEBxxEhEhBaDaCaD'
+fi
+
 zstyle ':dotzsh:module:theme:grepcolors:dotphiles' theme '37;45'
 
 # Patterns
