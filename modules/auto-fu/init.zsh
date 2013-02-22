270cc72d1059cd1d05071fc504fd218dbb3a1bfd
Ben O'Hara <bohara@gmail.com>
2013-02-23 00:05:43 +1000

    add header

--- modules/auto-fu/init.zsh
+++ modules/auto-fu/init.zsh
@@ -1,3 +1,11 @@
+#
+# dotzsh : https://github.com/dotphiles/dotzsh
+#
+# Setup auto-fu, automatic completion
+#
+# Authors:
+#   Ben O'Hara <bohara@gmail.com>
+
 source "${0:h}/external/auto-fu.zsh"
 
 zle-line-init () {auto-fu-init;}; zle -N zle-line-init
@@ -5,3 +13,4 @@ zle-line-init () {auto-fu-init;}; zle -N zle-line-init
 zstyle ':completion:*' completer _oldlist _complete
 
 zle -N zle-keymap-select auto-fu-zle-keymap-select
+
