a0e91fca519cee18411044b2b02987340eef1dfc
Ben O'Hara <bohara@gmail.com>
2013-04-01 17:57:05 +1000

    Fix path for brew install python

--- modules/python/init.zsh
+++ modules/python/init.zsh
@@ -16,7 +16,7 @@ fi
 # Prepend PEP 370 per user site packages directory, which defaults to
 # ~/Library/Python on Mac OS X and ~/.local elsewhere, to PATH/MANPATH.
 if [[ "$OSTYPE" == darwin* ]]; then
-  path=($HOME/Library/Python/*/bin(N) $path)
+  path=(/usr/local/ishare/python $HOME/Library/Python/*/bin(N) $path)
   manpath=($HOME/Library/Python/*/{,share/}man(N) $manpath)
 else
   # This is subject to change.
